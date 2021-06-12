09c1156ec3d7ace1ba684d57c9c872ba0fdee691
Aavas Sedhai <asedhai@gmail.com>
2021-06-12 12:00:31 +0545

    first files

---
+++ day1.py
@@ -0,0 +1,56 @@
+'''n1=int(input('enter a number'))
+n2=int(input('enter a number'))
+n3=int(input('enter a number'))
+N=n3+n2+n1
+print(f'sum= {N}')'''
+
+#area of a rt. angled triangle
+'''b=float(input('enter the length of base'))
+h=float(input('enter the length of height'))
+Area=(b*h)/2
+print(f'area={Area} cm^2')'''
+
+A=int(input('number of apples='))
+S=int(input('Number of students='))
+rem=A%S
+print(f'{rem} apples remain in the basket')
+app=A//S
+print(f'{app} apples are recieved by each')
+
+'''R=10**3
+print(R)'''
+
+'''carname="Volvo"
+print(carname)'''
+
+'''x=50
+print('x=',x)'''
+
+'''x=5
+y=10
+s=x+y
+print('x+y=',s)'''
+
+'''z='x+y'
+print(z)'''
+
+#myfirst_name="John"
+
+'''x,y,z="orange","orange","orange"#0r x=y=z='orange'
+print(x,y,z)'''
+
+'''a=int(input('enter a number'))
+b=int(input('enter another number'))
+if a>b:
+    print('Hello world')
+elif a!=b:
+    print('No hello world')
+elif a==b:
+    print(' yes')'''
+
+'''n=int(input('enter a number'))
+R=n%10
+print('Last digit=', R)
+f=n//10
+print('first digit=', f)'''
+
---
+++ main.py
@@ -0,0 +1,10 @@
+val1=float(input('Enter a number'))
+val2=float(input('Enter a second number'))
+ADD=val1+val2
+print(f' Adding:{ADD}')
+SUB=val1-val2
+print(f'Subtracting: {SUB}')
+MUL=val2*val1
+print( f'MULTIPLYING: {MUL}')
+DIV=val2/val1
+print( f'DIVIDING: {DIV}')
