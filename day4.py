a42747d1d6f41b6d91dfad3fd54750fb44dae3c0
Aavas Sedhai <asedhai@gmail.com>
2021-06-21 14:22:30 +0545

    day 4

---
+++ day4.py
@@ -0,0 +1,63 @@
+#7
+'''N=input('enter a number')
+sum=0
+for i in N:
+    i=int(i)
+    sum=sum+i
+print(f'sum of the digits of {N} is {sum}')'''
+
+#6
+'''N=int(input('give a number'))
+ld=N%10
+print(f'last digit={ld}')'''
+
+#5
+'''a,b,c=int(input('Enter a number')),int(input('Enter a number')),int(input('Enter a number'))
+if a<b and a<c:
+    print(f'{a} is the smallest')
+elif b<a and b<c:
+    print(f'{b} is the smallest')
+else:
+    print(f'{c} is the smallest')'''
+
+#4
+'''l=[1,2,3,4,5]
+for i in l:
+    if i==5 and i in l:
+        print('5 is in the list of first 5 natural numbers')'''
+
+#3
+'''A,B=int(input('Number of students=')),int(input('Number of apples='))
+APP=A//B
+print(f'{APP} apples are receives by each')
+REM=B%A
+print(f'{REM} apples remain in the basket')'''
+
+#2
+'''KERA='banana'
+kera='BANANA'
+a=KERA>kera
+print(a)#true'''
+
+#1
+'''stu1=int(input('Students in class A='))
+cha1=stu1//2
+if stu1%2!=0:
+    cha1=cha1+1
+print(f'{cha1} chairs are required')
+stu2=int(input('Students in class B='))
+cha2=stu2//2
+if stu2%2!=0:
+    cha2=cha2+1
+print(f'{cha2} chairs are required')
+stu3=int(input('Students in class C='))
+cha3=stu3//2
+if stu3%2!=0:
+    cha3=cha3+1
+print(f'{cha3} chairs are required')
+C=cha3+cha2+cha1
+print(f'Total {C} chairs are needed')'''
+
+
+
+
